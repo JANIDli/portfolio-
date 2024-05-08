93bd7766a76cc9f51a719c5e2e570498ae7b7dd7
JANIDli <G15916.janitha@gmail.com>
2024-05-08 17:13:15 +0530

    about

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './styles.css';
 import './App.css'
 import Home from "./component/Page/Home";
+import About from './component/About';
 
 
 function App() {
   return (
     <dir>
       <Home/>
+      <About/>
     </dir>
   );
 }
---
+++ src/component/About.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export class About extends Component {
+  render() {
+    return (
+      <div className='about-container'>
+        <div className="about-img">
+            <img src="about.png" alt="" />
+        </div>
+        <div className="about-text">
+            <h2>ABOUT</h2>
+            <p>Hello! I'm Janitha, a passionate web developer and graphic designer with a keper and graphic designer with a keper and graphic designer with a keper and graphic designer with a keper and graphic designer with a keper and graphic designer with a keen eye for detail and a drive for creative excellence. With a background in both web development and graphic design, I bring a unique blend of technical skills and artistic vision to every project I undertake.</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default About
